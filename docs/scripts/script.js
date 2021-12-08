8de6334176fdba48530981067d9d4e2adeb67281
schung-21 <schung21@artic.edu>
2021-12-08 17:46:35 -0600

    update

---
+++ docs/scripts/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $(".tab").dialog({
+        autoOpen: false,
+        closeText: "",
+        width: 1000,
+        maxHeight: 600
+    });
+
+    $(".tab.design").dialog({width: 600});
+    $(".tab.art").dialog({width: 500});
+    $(".tab#calendar").dialog({width: 800});
+
+    function opentab(id) {
+        var button = ".folder" + id;
+        var tab = ".tab" + id;
+        $(button).click(function() {
+            $(tab).dialog("open");
+        });
+    };
+    opentab(".design");
+    opentab(".art");
+    opentab(".about");
+
+    opentab("#wa");
+    opentab("#mca");
+    opentab("#be");
+    opentab("#jam");
+
+    opentab("#calendar");
+    opentab("#ds");
+    opentab("#moma");
+    opentab("#playlist");
+});
